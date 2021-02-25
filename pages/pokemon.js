d69601a44770e77520e5ecfb3c8371be207a3d4f
samuel façanha <samufacanha@gmail.com>
2021-02-25 16:07:58 -0300

    v0.3 - individual cards working - cleaner

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import axios from 'axios'
 
 export default function Pokemon({pokemon}) {
 
     console.log(pokemon);
-    let a=0;
+
         return (
         <div >
             <Head>
@@ -22,17 +21,17 @@ export default function Pokemon({pokemon}) {
                 <img src={`http://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemon.id}.png?raw=true`} id="PokemonImg"/>
                 <h1 className="text-center text-4xl capitalize"> {pokemon.name} </h1>
                 <br/>
-                <p className="text-2xl text-left">
+                <div className="text-2xl text-left">
                     Type(s):
                     {pokemon.types.map((type, index)=> (<p key={index} className=" text-xl capitalize"> {type.type.name}</p>))}
-                </p>
+                </div>
                 <br/>
-                <p className="text-2xl text-left">
+                <div className="text-2xl text-left">
                     Abilities:
-                    {pokemon.abilities.map((abil, index, isHidden)=> abil.is_hidden?
+                    {pokemon.abilities.map((abil, index)=> abil.is_hidden?
                     (<p key={index} className=" text-xl capitalize text-red-500"> {abil.ability.name} (Hidden Ability)</p>):
                     (<p key={index} className=" text-xl capitalize"> {abil.ability.name}</p>))}
-                </p>
+                </div>
             </div>
             </div>
             </div>
